099f9531b1eadacaf558fbee93f3e341220ce75e
sanjvj <sanjaysurya3010@gmail.com>
2024-07-26 00:07:47 +0530

    About Section added

--- frontend/src/Components/About.jsx
+++ frontend/src/Components/About.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="mt-20 mx-52"><h1 className="text-6xl">About Us</h1>
+    <div className="mt-20 mx-52"><h1 className="text-6xl mb-10 mt-20">About Us</h1>
     <div className=" flex gap-24">
     <div>
       <img src="offset -1.jpg" alt="Offset Printer" className="h-auto w-auto rounded-xl"></img>
     </div>
       <div>
         
-        <h1 className="text-xl">
+        <h1 className="text-3xl font-medium">
           Our company, "THE SARASWATHI OFFSET PRINTERS, " was established as a
           sister concern to Sri Krishna Fireworks in 1968. Over time, we evolved
           to support external clients by providing high-quality packaging
--- frontend/src/Components/Hero.jsx
+++ frontend/src/Components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="relative m-10 h-[80vh] rounded-3xl overflow-hidden">
+    <div className="relative mx-10 my-7 h-[82vh] rounded-3xl overflow-hidden">
       <div className="absolute inset-0 bg-custom-image bg-cover bg-center filter brightness-[0.40]"></div>
       <div className="relative z-10 flex flex-col gap-5 items-start ml-40 mr-60 justify-start my-52 h-full text-white">
         <h1 className="text-8xl font-bold">
