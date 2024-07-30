a5e5ca2b029f4719acd27e19bd2402f2960ea1a9
sanjvj <sanjaysurya3010@gmail.com>
2024-07-30 23:37:11 +0530

    Added Workflow

--- frontend/src/Components/Workflow.jsx
+++ frontend/src/Components/Workflow.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Workflow = () => {
   return (
-    <div>
-      
+    <div className="bg-black">
+      <h1 className="text-8xl bg-gradient-to-r from-sky-400 to-pink-400 bg-clip-text text-transparent  py-10 text-center font-bold">
+        Walk through our Workflow
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Workflow
+export default Workflow;
