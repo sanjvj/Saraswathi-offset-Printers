99b32eb561ad169b1e6520585f2d408419bb4554
sanjvj <sanjaysurya3010@gmail.com>
2024-08-08 01:20:24 +0530

    About us and Workflow Changes

--- frontend/src/Components/About.jsx
+++ frontend/src/Components/About.jsx
@@ -5,75 +5,75 @@ const About = () => {
   return (
     <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 relative mt-20'>
     <h1 className='text-center font-semibold text-4xl'>Here's why Saraswathi Printers is a right fit for you</h1>
-      <div className="grid grid-cols-1 lg:grid-cols-2 mt-32 gap-20 mb-52 ">
+      <div className="grid grid-cols-1 lg:grid-cols-2 mt-32 gap-10 mb-52 ">
         <motion.div
-          whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: -100 }}
-          transition={{ duration: 1.5 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 100 }}
+          transition={{ duration: 0.5 }}
           className="col-span-2 lg:col-span-1"
         >
           <img className="rounded-lg" src="offset-1.jpg" alt="About Us" />
         </motion.div>
 
         <motion.div
-          whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: 100 }}
-          transition={{ duration: 1.5 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 100 }}
+          transition={{ duration: 0.5 }}
           className="col-span-2 lg:col-span-1 flex flex-col justify-center"
         >
           <h1 className="text-4xl font-semibold pb-1">About Us</h1>
           <br />
-          <p className="font-normal text-xl text-gray-600">
+          <p className="font-[400] text-[18px] text-gray-700">
             At Saraswathi Offset Printers, we are dedicated to delivering high-quality printing solutions since 1968. Starting as a sister concern to Sri Krishna Fireworks, we now serve a diverse range of clients with our advanced printing solutions. Our commitment to precision, innovation, and customer satisfaction ensures that we meet your needs with unparalleled quality and efficiency.
           </p>
           <br />
         </motion.div>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 mt-28 gap-20 mb-52">
+      <div className="grid grid-cols-1 lg:grid-cols-2 mt-28 gap-10 mb-52">
         <motion.div
-          whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: -100 }}
-          transition={{ duration: 1 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 100 }}
+          transition={{ duration: 0.5 }}
           className="col-span-2 lg:col-span-1 flex flex-col justify-center"
         >
           <h1 className="text-4xl font-semibold pb-1">Our Mission</h1>
           <br />
-          <p className="font-light text-lg text-gray-700">
+          <p className="font-light text-xl text-gray-700">
             Saraswathi Offset Printers is dedicated to providing high-quality, innovative, and sustainable printing solutions. We prioritize exceptional customer service, cutting-edge technology, and a supportive work environment to consistently exceed client expectations and build lasting relationships.
           </p>
           <br />
         </motion.div>
 
         <motion.div
-          whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: 100 }}
-          transition={{ duration: 1 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 100 }}
+          transition={{ duration: 0.5 }}
           className="col-span-2 lg:col-span-1"
         >
           <img className="rounded-lg" src="offset-2.jpg" alt="Our Mission" />
         </motion.div>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 mt-32 gap-20">
+      <div className="grid grid-cols-1 lg:grid-cols-2 mt-32 gap-10">
         <motion.div
-          whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: -100 }}
-          transition={{ duration: 1.5 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 100 }}
+          transition={{ duration: 0.5 }}
           className="col-span-2 lg:col-span-1"
         >
           <img className="rounded-lg" src="offset-3.jpg" alt="Our Vision" />
         </motion.div>
 
         <motion.div
-          whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: 100 }}
-          transition={{ duration: 1.5 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 100 }}
+          transition={{ duration: 0.5 }}
           className="col-span-2 lg:col-span-1 flex flex-col justify-center"
         >
           <h1 className="text-4xl font-semibold pb-1">Our Vision</h1>
           <br />
-          <p className="font-light text-lg text-gray-700">
+          <p className="font-light text-xl text-gray-700">
             Our vision is to lead the industry in innovative and sustainable printing solutions, delivering unmatched quality and customer satisfaction. We are dedicated to advancing our technology and processes, fostering a culture of excellence, and creating lasting value for our clients and community through responsible practices.
           </p>
           <br />
--- frontend/src/Components/Workflow.jsx
+++ frontend/src/Components/Workflow.jsx
@@ -37,14 +37,16 @@ const WorkflowAnimation = () => {
   return (
     <div className="min-h-screen flex items-center justify-center relative">
       <div className="text-center">
-        <h1 className="text-6xl mb-10">Walk through our Workflow</h1>
+        <h1 className="text-6xl mb-20 mt-32">Walk through our Workflow</h1>
         <div className="relative max-w-5xl mx-auto" ref={ref}>
-          <svg id="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000" className="svg-container">
+          <svg id="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 1000" className="svg-container">
+            {/* Define a vertical line path */}
             <motion.path
-              d="M 200 150 V 300 H 600 V 300 H 600 V 600 H 200 V 600 H 200 V 750 "
+              d="M 600 25 V 2000" // Vertical line from y=50 to y=950
               fill="none"
               stroke="black"
               strokeWidth="5px"
+              strokeDasharray="5,5"
               initial="hidden"
               animate={controls}
               variants={lineVariants}
@@ -56,7 +58,7 @@ const WorkflowAnimation = () => {
             initial="hidden"
             animate={controls}
             variants={boxVariants}
-            style={{ top: '0px', left: '50px' }}
+            style={{ top: '50px', left: '180px' }} // Adjusted position
           >
             <h2>Pre-Press</h2>
             <p>Utilizing design software such as CorelDRAW to import printed files onto CTP plates.</p>
@@ -66,7 +68,7 @@ const WorkflowAnimation = () => {
             initial="hidden"
             animate={controls}
             variants={boxVariants}
-            style={{ top: '350px', left: '550px' }}
+            style={{ top: '400px', left: '550px' }} // Adjusted position
           >
             <h2>Press</h2>
             <p>Printing tasks for CPC, P14, P24, and P28 will be executed at this facility.</p>
@@ -76,7 +78,7 @@ const WorkflowAnimation = () => {
             initial="hidden"
             animate={controls}
             variants={boxVariants}
-            style={{ top: '685px', left: '50px' }}
+            style={{ top: '750px', left: '180px' }} // Adjusted position
           >
             <h2>Post-Press</h2>
             <p>Job finishing tasks, such as cutting, die cutting, and scoring, will be carried out at this facility.</p>
