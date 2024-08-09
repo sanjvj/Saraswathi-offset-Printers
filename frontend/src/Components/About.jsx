de2a1483a4dfde59b145ba617fc69e96016b0ae2
sanjvj <sanjaysurya3010@gmail.com>
2024-08-09 20:14:43 +0530

    About change

--- frontend/src/Components/About.jsx
+++ frontend/src/Components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 relative mt-20'>
     <h1 className='text-center font-semibold text-4xl'>Here's why Saraswathi Printers is a right fit for you</h1>
-      <div className="grid grid-cols-1 lg:grid-cols-2 mt-32 gap-10 mb-52 ">
+      <div className="grid grid-cols-1 lg:grid-cols-2 mt-32 gap-10 ">
         <motion.div
           whileInView={{ opacity: 1, y: 0 }}
           initial={{ opacity: 0, y: 100 }}
@@ -30,7 +30,7 @@ const About = () => {
         </motion.div>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 mt-28 gap-10 mb-52">
+      <div className="grid grid-cols-1 lg:grid-cols-2 mt-28 gap-10 ">
         <motion.div
           whileInView={{ opacity: 1, y: 0 }}
           initial={{ opacity: 0, y: 100 }}
@@ -55,7 +55,7 @@ const About = () => {
         </motion.div>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 mt-32 gap-10">
+      <div className="grid grid-cols-1 lg:grid-cols-2 mt-32 gap-10 mb-32">
         <motion.div
           whileInView={{ opacity: 1, y: 0 }}
           initial={{ opacity: 0, y: 100 }}
@@ -69,7 +69,7 @@ const About = () => {
           whileInView={{ opacity: 1, y: 0 }}
           initial={{ opacity: 0, y: 100 }}
           transition={{ duration: 0.5 }}
-          className="col-span-2 lg:col-span-1 flex flex-col justify-center"
+          className="col-span-2 lg:col-span-1 flex flex-col justify-center "
         >
           <h1 className="text-4xl font-semibold pb-1">Our Vision</h1>
           <br />
